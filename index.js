1676a63daea92f8c23f3eb5793d2f2c1c95c711e
anfaal <anfx369@gmail.com>
2024-10-01 21:53:53 +0530

    initiated simple nodejs app

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Home route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Simple Express App!');
+});
+
+// About route
+app.get('/about', (req, res) => {
+    res.send('This is a simple Express application.');
+});
+
+// API route to echo back the request body
+app.post('/echo', (req, res) => {
+    res.json(req.body);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
